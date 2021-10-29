f7d14a8369aece411d8a488295733f039776b0b0
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-10-29 15:15:29 +0700

    Initial commit

---
+++ src/main/java/com/anhnbt/jenkins/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.anhnbt.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nguyen Ba Tuan Anh <anhnbt.it@gmail.com>
+ * Date: 10/29/2021
+ * Time: 3:13 PM
+ */
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Hello World!";
+    }
+}
