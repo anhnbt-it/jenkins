850ba76317757a8f40b469ee06f51a55d59b9a66
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-10-29 15:27:14 +0700

    refactor: change method name

--- src/main/java/com/anhnbt/jenkins/controller/HomeController.java
+++ src/main/java/com/anhnbt/jenkins/controller/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public String home() {
+    public String greetings() {
         return "Hello World!";
     }
 }
